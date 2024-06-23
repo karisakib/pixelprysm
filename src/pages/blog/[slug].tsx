bc889c82cc1f05b2b5eea89f010094b918e2b024
Kari Sakib <karisakib@gmail.com>
2024-06-23 17:47:41 -0400

    added decap cms

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,13 @@
+import Layout from "@/layouts/Layout";
+
+export default function BlogPost() {
+ return (
+   <Layout>
+    <div className="flex flex-col pr-16">
+      <h2 className="text-3xl font-semibold tracking-tight">
+       Slugged blog appears here
+      </h2>
+     </div>
+   </Layout>
+ );
+}
